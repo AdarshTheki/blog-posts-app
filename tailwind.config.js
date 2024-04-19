f07cd68fd3d0fe359d658d3a6f4751604623476d
adarshtheki <adarshverma549@gmail.com>
2024-04-19 16:00:36 +0530

    Update dependencies, colors, and fonts in Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 export default {
     content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            screens: {
+                sm: '480px',
+                md: '768px',
+                lg: '976px',
+                xl: '1440px',
+            },
+            colors: {
+                primary: '#FF5193',
+                secondary: '#0095F1',
+                dark: '#212544',
+                light: '#e1e0f1',
+                white: '#fff',
+            },
+            fontFamily: {
+                sans: ['Roboto', 'sans-serif'],
+                serif: ['DM Sans', 'serif'],
+                body: ['DM Sans', 'serif'],
+            },
+        },
     },
     plugins: [],
 };
