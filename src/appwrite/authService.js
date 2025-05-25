43910a695ec137c341c5c88ddae6a68715ad3346
Adarshtheki <adarshverma549@gmail.com>
2025-05-25 16:24:09 +0530

    env config file chnage

--- src/appwrite/authService.js
+++ src/appwrite/authService.js
@@ -16,7 +16,7 @@ export class AuthServices {
     try {
       const userId = ID.unique();
       const userAccount = await this.account.create(userId, email, password, name);
-      if (userAccount?.$id) {
+      if (userAccount.$id) {
         return this.login({ email, password });
       } else {
         return userAccount;
--- src/constant.js
+++ src/constant.js
@@ -5,5 +5,5 @@ export const config = {
   appwriteCollectionWork: import.meta.env.VITE_APPWRITE_COLLECTION_WORKS_ID || '',
   appwriteCollectionBlog: import.meta.env.VITE_APPWRITE_COLLECTION_BLOGS_ID || '',
   appwriteBucket: import.meta.env.VITE_APPWRITE_BUCKET_ID || '',
-  TinyApiKey: import.meta.env.VITE_TINY_API_KEY || '',
+  TinyApiKey: import.meta.env.VITE_TINY_API_URL || '',
 };
